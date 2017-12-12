a3e988d5c4d885a86950f4ee3b652723ecad2008
Robert Sköld <robert@publicclass.se>
2017-12-12 17:26:35 +0100

    Made the split change backwards compatible

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 export * from "./Query.js";
 export * from "./Mutate.js";
 export * from "./Provider.js";
+
+// for backwards compatibility
+export { ApolloClient as MockClient } from "./mock/ApolloClient.js";
+export * from "./state/renderState.js";
